12f3cc1479024b746ec9ae67ca3f9847118f6798
Дмитрий Захаров <zds.vip1221@gmail.com>
2024-03-12 14:58:16 +0300

    e

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product_page, go_to_product, registration, make_product
 
 urlpatterns = [
     path('', home),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('product/<int:product_id>/', product_page, name='product_page'),
+    path('go_to_product/', go_to_product, name='go_to_product'),
+    path('register/', registration),
+    path('create_product/', make_product)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,43 +1,67 @@
 import json
-
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from catalog.models import Users
+from catalog.models import Users, Products, Categories
 
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    categories = list(Categories.objects.all())
+    products = list(Products.objects.all())
+    context = {
+                "products": products,
+                "categories": categories
+               }
+    return render(request, 'home.html', context)
+
 
 def contacts(request):
-    contact_information = {}
-    name = request.POST.get('name')
-    phone = request.POST.get('phone')
-    message = request.POST.get('message')
-    contact_information = {
-                            'name':name,
-                           'phone': phone,
-                           'message': message
-                           }
-    print(contact_information)
-    return render(request, 'contacts.html')
+    categories = list(Categories.objects.all())
 
-# def post(self, request):
-#
-#     user_data = json.loads(request.body)
-#
-#     user = Users()
-#
-#     user.name = user_data.get('name')
-#     user.phone = user_data.get('phone')
-#     user.message = user_data.get('message')
-#
-#     user.save()
-#
-#     return JsonResponse({
-#         'name': user.name,
-#         'phone': user.phone,
-#         'message': user.message
-#     })
+    context = {
+        "categories": categories
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        contact_information = {
+                                'name': name,
+                                'phone': phone,
+                                'message': message
+                               }
+        print(contact_information)
+    return render(request, 'contacts.html', context)
+
+
+def product_page(request, product_id):
+    product_info = Products.objects.filter(id=product_id).first()
+    context = {'product_info': product_info}
+    return render(request, 'product.html', context)
+
+
+def go_to_product(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        print('OK')
+        return redirect('product_page', product_id=product_id)
+    return redirect('home')
+
+
+def registration(request):
+    categories = list(Categories.objects.all())
+    context = {'categories': categories}
+    return render(request, 'registartion.html', context)
+
+
+def make_product(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'create_product.html')
+
+
+
+
